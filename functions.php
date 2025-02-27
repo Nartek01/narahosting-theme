1ddd495800efaa8e62e04711310697609a3c05a5
Master Nara Lundmalm <52786849+Nartek01@users.noreply.github.com>
2025-02-27 10:45:59 +0100

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+function narahosting_block_init() {
+	register_block_type( __DIR__ . '/build/myheader' );
+	register_block_type( __DIR__ . '/build/myfooter' );
+	register_block_type( __DIR__ . '/build/narahostingCta' );
+	register_block_type( __DIR__ . '/build/narahostingHeroBlock' );
+	register_block_type( __DIR__ . '/build/narahostingLatestPosts' );
+}
+add_action( 'init', 'narahosting_block_init' );
+
+function theme_enqueue_styles() {
+    wp_enqueue_style('narahosting-main-scss', get_template_directory_uri() . '/style.scss');
+}
+add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
