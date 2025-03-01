03a8350389461f5a0e66699a6ad55b6879b26e32
Master Nara Lundmalm <nartek01@gmail.com>
2025-03-01 21:26:25 +0100

    Updated styling of narahostingCodeBlock

--- build/narahostingCodeBlock/index.asset.php
+++ build/narahostingCodeBlock/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '5bf15bc40dad1facf171');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '8a4eaa98601aac79910d');
