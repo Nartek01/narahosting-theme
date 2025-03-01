256c38970c50f94726f226948f1b85bdfb756b41
Master Nara Lundmalm <nartek01@gmail.com>
2025-03-01 21:38:18 +0100

    Moved grid-template-columns: 1fr auto !important; into proper parent element

--- build/narahostingCodeBlock/index.asset.php
+++ build/narahostingCodeBlock/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'e7abbe34263aab34c85a');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '1dbd4e3de33c32d2704d');
