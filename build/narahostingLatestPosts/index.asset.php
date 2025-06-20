be21a79be1b45d318b018b557678097c873061c2
Nara <you@exmaple.com>
2025-06-20 20:41:52 +0200

    build: compile assets for production

--- build/narahostingLatestPosts/index.asset.php
+++ build/narahostingLatestPosts/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-i18n'), 'version' => '2a9aa758c3daad2386ca');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-i18n'), 'version' => 'd90c86e8b8ef97e51081');
